26f95e7966408256b3f1d10f5c6c69e1f55a6193
EvgeniyBelog <85622172+EvgeniyBelog@users.noreply.github.com>
2021-06-27 22:40:18 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$(function(){
+$('.reviews__slider').slick({
+ arrows:false,
+ slidesToShow: 3,
+ slidesToScroll: 3,
+ autoplay: true,
+ autoplaySpeed: 2000,
+ responsive: [
+  {
+    breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        }
+       },
+        {
+     breakpoint: 521,
+       settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        }       
+  }]
+});
+
+});
